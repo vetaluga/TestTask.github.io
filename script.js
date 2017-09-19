d27c76ebe5eb3419722b50c06a1e87150e1a449b
vetaluga <vetalugan@mail.ru>
2017-09-20 01:23:54 +0300

    adding-change-0.4.2
    Change image girl.
    Added google map.

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ $(document).ready(function(){
       $('.img-feedback-2').show();
     });
 });
+
+function myMap() {
+  var myCenter = new google.maps.LatLng(49.2674, 28.50149327);
+  var mapCanvas = document.getElementById("map");
+  var mapOptions = {center: myCenter, zoom: 15};
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+  var marker = new google.maps.Marker({position:myCenter});
+  marker.setMap(map);
+
+  // Zoom to 9 when clicking on marker
+  google.maps.event.addListener(marker,'click',function() {
+    map.setZoom(17);
+    map.setCenter(marker.getPosition());
+  });
+}
